3dcc593175228c4feff32f2a43afcde9de42f092
Alec Hall <alecnhall@gmail.com>
2019-08-05 21:15:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(arr)
   cheeses = ["cheddar", "gouda", "camembert"]
   arr.map do |i| 
-    if i.include?()
+    if i.include?("cheddar")
       p i
     else 
       p nil
